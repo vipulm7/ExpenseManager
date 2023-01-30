97ccb5cd4ffe14b0f699ff3ec4e1c7caa173bb37
Vipul Mittal <20001003137@jcboseust.ac.in>
2023-01-30 18:23:37 +0530

    gradle updated

--- app/src/main/java/com/VipulMittal/expensemanager/MainActivity.java
+++ app/src/main/java/com/VipulMittal/expensemanager/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.biometric.BiometricPrompt;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -19,6 +20,7 @@ import androidx.preference.SwitchPreference;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.ActivityOptions;
 import android.app.AlarmManager;
 import android.app.AlertDialog;
@@ -27,6 +29,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
@@ -43,7 +46,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -97,15 +99,14 @@ public class MainActivity extends AppCompatActivity implements Serializable {
 	public CategoryFragment categoryFragment;
 	View accView, catView;
 	boolean b1, b2, b3, b4;
-	RadioGroup rg_catDialog;
 	public long systemTimeInMillies;
 	boolean first_time;
 	public static int[] amount;
 	public static int sum_amounts;
 
 	public static NotificationManagerCompat notificationManager;
-	public static final String ACTION_SNOOZE="com.example.android.wearable.wear.wearnotifications.handlers.action.SNOOZE";
-	public static final String ACTION_DISMISS="com.example.android.wearable.wear.wearnotifications.handlers.action.DISMISS";
+//	public static final String ACTION_SNOOZE="com.example.android.wearable.wear.wearnotifications.handlers.action.SNOOZE";
+//	public static final String ACTION_DISMISS="com.example.android.wearable.wear.wearnotifications.handlers.action.DISMISS";
 	public final String CHANNEL_ID="1";
 	public static final int notifID=2;
 	ActivityResultLauncher<Intent> abc;
@@ -135,6 +136,14 @@ public class MainActivity extends AppCompatActivity implements Serializable {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		System.out.println("hi");
+
+		if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+			Log.d(TAG, "onCreate: granted");
+		else {
+			Log.d(TAG, "onCreate: not granted");
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 124);
+		}
 
 		FABAdd=findViewById(R.id.FABAdd);
 		navigationBarView=findViewById(R.id.BottomNavigation);
@@ -270,27 +279,24 @@ public class MainActivity extends AppCompatActivity implements Serializable {
 
 		navigationBarView.setSelectedItemId(R.id.bn_home);
 
-		navigationBarView.setOnItemReselectedListener (new NavigationBarView.OnItemReselectedListener() {
-			@Override
-			public void onNavigationItemReselected(@NonNull MenuItem item) {
-				int id=item.getItemId();
-				String print;
+		navigationBarView.setOnItemReselectedListener (item -> {
+			int id=item.getItemId();
+			String print;
 
-				if(id==R.id.bn_home)
-					print="Home";
-				else if(id==R.id.bn_cat)
-					print="Category";
-				else if(id==R.id.bn_accounts)
-					print="Accounts";
-				else
-					print="Analysis";
+			if(id==R.id.bn_home)
+				print="Home";
+			else if(id==R.id.bn_cat)
+				print="Category";
+			else if(id==R.id.bn_accounts)
+				print="Accounts";
+			else
+				print="Analysis";
 
-				if(toast!=null)
-					toast.cancel();
+			if(toast!=null)
+				toast.cancel();
 
-				toast=Toast.makeText(MainActivity.this, print, Toast.LENGTH_SHORT);
-				toast.show();
-			}
+			toast=Toast.makeText(MainActivity.this, print, Toast.LENGTH_SHORT);
+			toast.show();
 		});
 
 		LayoutInflater layoutInflater=LayoutInflater.from(this);
