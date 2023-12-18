e949ab7ad9d280b27a30f9facea0d802aa2e69d0
vipulm7 <20001003137@jcboseust.ac.in>
2023-12-18 05:58:45 +0530

    BackupRestoreFragment

--- app/src/main/java/com/VipulMittal/expensemanager/BackupRestoreFragment.java
+++ app/src/main/java/com/VipulMittal/expensemanager/BackupRestoreFragment.java
@@ -1,26 +1,41 @@
 package com.VipulMittal.expensemanager;
 
-import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.VipulMittal.expensemanager.accountRoom.AccountDatabase;
+import com.VipulMittal.expensemanager.categoryRoom.CategoryDatabase;
+import com.VipulMittal.expensemanager.subCategoryRoom.SubCategoryDatabase;
+import com.VipulMittal.expensemanager.transactionRoom.TransactionDatabase;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Calendar;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class BackupRestoreFragment extends PreferenceFragmentCompat {
 
 	public static final String TAG = "Vipul_tag";
 	MainActivity mainActivity;
+	ActivityResultLauncher<Intent> exportDBLauncher, importDBLauncher;
 
 	@Override
 	public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
@@ -28,123 +43,164 @@ public class BackupRestoreFragment extends PreferenceFragmentCompat {
 
 		Preference backupPreference = findPreference("backup");
 		Preference restorePreference = findPreference("restore");
-		mainActivity = (MainActivity) getActivity();
+		mainActivity = (MainActivity) requireActivity();
+		mainActivity.actionBar.setDisplayHomeAsUpEnabled(true);
 
-		backupPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(@NonNull Preference preference) {
+		assert backupPreference != null;
+		assert restorePreference != null;
 
-//				File(mainActivity.accountViewModel.repo.accountDatabase.getOpenHelper().getWritableDatabase().getPath())
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.O)
+			return;
 
+		backupPreference.setOnPreferenceClickListener(preference -> {
+			try {
+				Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+				intent.addCategory(Intent.CATEGORY_OPENABLE);
+				intent.setType("application/zip");
+				intent.putExtra(Intent.EXTRA_TITLE, "ExpenseManager_db" + ".zip");
+				exportDBLauncher.launch(intent);
+			} catch (Exception e) {
+				Log.d(TAG, "exportDatabase: error = " + e);
+				Toast.makeText(requireContext(), "Backup not created", Toast.LENGTH_LONG).show();
+			}
 
-				int permission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+			return true;
+		});
 
-				if (true) {
-					File folder = new File(Environment.getExternalStorageDirectory() + File.separator + "Expense Tracker");
+		restorePreference.setOnPreferenceClickListener(preference -> {
+			Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+			intent.addCategory(Intent.CATEGORY_OPENABLE);
+			intent.setType("application/zip");
+			importDBLauncher.launch(intent);
 
-					if (!folder.exists()) {
-						if (!folder.mkdirs()) {
-							if (mainActivity.toast != null)
-								mainActivity.toast.cancel();
-							mainActivity.toast = Toast.makeText(getContext(), "Can't create directory!", Toast.LENGTH_SHORT);
-							mainActivity.toast.show();
-						}
-					}
+			return true;
+		});
 
-					File sd = Environment.getExternalStorageDirectory();
-					File data = Environment.getDataDirectory();
+		exportDBLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+			if (result.getResultCode() == Activity.RESULT_OK) {
+				Intent intent = result.getData();
+				if (intent != null) {
+					Uri uri = intent.getData();
+					if (uri != null) {
+						backupDatabase(uri);
+						Toast.makeText(requireContext(), "Backup created", Toast.LENGTH_LONG).show();
+					} else
+						Toast.makeText(requireContext(), "Backup not created", Toast.LENGTH_LONG).show();
+				} else
+					Toast.makeText(requireContext(), "Backup not created", Toast.LENGTH_LONG).show();
+			} else
+				Toast.makeText(requireContext(), "Backup not created", Toast.LENGTH_LONG).show();
+		});
 
-					if (sd.canWrite()) {
-						String currentDBPath = "//data//ExpenseTracker//databases//trans_database";
-						String backupDBPath = "/BackupFolder/trans_database";
-						File currentDB = new File(data, currentDBPath);
-						File backupDB = new File(sd, backupDBPath);
-
-						try {
-							FileChannel src = new FileInputStream(currentDB).getChannel();
-							FileChannel dst = new FileInputStream(backupDB).getChannel();
-
-							dst.transferFrom(src, 0, src.size());
-							src.close();
-							dst.close();
-
-							if (mainActivity.toast != null)
-								mainActivity.toast.cancel();
-							mainActivity.toast = Toast.makeText(getContext(), "Backup created successfully!", Toast.LENGTH_SHORT);
-							mainActivity.toast.show();
-
-						} catch (FileNotFoundException e) {
-							e.printStackTrace();
-							mainActivity.toast = Toast.makeText(getContext(), e.toString(), Toast.LENGTH_LONG);
-							mainActivity.toast.show();
-						} catch (IOException e) {
-							e.printStackTrace();
-							mainActivity.toast = Toast.makeText(getContext(), e.toString(), Toast.LENGTH_LONG);
-							mainActivity.toast.show();
-						}
-					} else {
-						mainActivity.toast = Toast.makeText(getContext(), "Can't write!", Toast.LENGTH_LONG);
-						mainActivity.toast.show();
-					}
-
-//
-//					File db= mainActivity.getDatabasePath("account_database");
-//					File dbShm = new File(db.getParent(), "dbShm");
-//					File dbWal = new File(db.getParent(), "dbWal");
-//
-//					File db2 = new File("/sdcard/", "");
-//					File dbShm2 = new File(db2.getParent(), "dbShm");
-//					File dbWal2 = new File(db2.getParent(), "dbWal");
-//
-//					try {
-//
-//					} catch (Exception e) {
-//						Log.e(TAG, "onPreferenceClick: " );
-//					}
-
-
-//					new Backup.Init()
-//							.database(mainActivity.accountViewModel.repo.accountDatabase)
-//							.path("/sdcard/")
-//							.fileName("acc.txt")
-//							.onWorkFinishListener(new OnWorkFinishListener() {
-//								@Override
-//								public void onFinished(boolean success, String message) {
-//									if (mainActivity.toast != null)
-//										mainActivity.toast.cancel();
-//									mainActivity.toast = Toast.makeText(getContext(), "Backup done!", Toast.LENGTH_SHORT);
-//									mainActivity.toast.show();
-//								}
-//							})
-//							.execute();
-				} else {
-					if (mainActivity.toast != null)
-						mainActivity.toast.cancel();
-					mainActivity.toast = Toast.makeText(getContext(), "No write permission!", Toast.LENGTH_SHORT);
-					mainActivity.toast.show();
+		importDBLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+			if (result.getResultCode() == Activity.RESULT_OK) {
+				Intent intent = result.getData();
+				if (intent != null) {
+					Uri uri = intent.getData();
+					restoreDatabases(uri);
 				}
-
-//				new Backup.Init()
-//				.database(mainActivity.accountViewModel.repo.accountDatabase)
-//				.path("/sdk_gphone_x86_64_arm64/")
-//				.fileName("acc.txt")
-//				.onWorkFinishListener((success, message) -> {
-//					if (mainActivity.toast != null)
-//						mainActivity.toast.cancel();
-//					mainActivity.toast = Toast.makeText(getContext(), "Backup done!", Toast.LENGTH_SHORT);
-//					mainActivity.toast.show();
-//				})
-//				.execute();
-
-				return true;
 			}
 		});
 
-		restorePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(@NonNull Preference preference) {
-				return false;
-			}
+	}
+
+	private void addDatabaseToZip(String databaseName, ZipOutputStream zipOutputStream) {
+		try {
+			String databasePath = requireActivity().getDatabasePath(databaseName).getAbsolutePath();
+			InputStream inputStream = Files.newInputStream(Paths.get(databasePath));
+			ZipEntry zipEntry = new ZipEntry(databaseName + ".db");
+			zipOutputStream.putNextEntry(zipEntry);
+
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = inputStream.read(buffer)) > 0)
+				zipOutputStream.write(buffer, 0, length);
+
+			zipOutputStream.closeEntry();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void backupDatabase(Uri uri) {
+
+		try {
+			OutputStream outputStream = requireActivity().getContentResolver().openOutputStream(uri);
+			ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
+
+			addDatabaseToZip("category_database", zipOutputStream);
+			addDatabaseToZip("subcat_database", zipOutputStream);
+			addDatabaseToZip("account_database", zipOutputStream);
+			addDatabaseToZip("trans_database", zipOutputStream);
+
+			zipOutputStream.flush();
+			zipOutputStream.finish();
+			zipOutputStream.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void restoreDatabase(ZipInputStream zipInputStream, String entryName) {
+		try {
+			File dbFile = requireActivity().getDatabasePath(entryName);
+			OutputStream outputStream = Files.newOutputStream(dbFile.toPath());
+			byte[] buffer = new byte[1024];
+			int length;
+
+			while ((length = zipInputStream.read(buffer)) > 0)
+				outputStream.write(buffer, 0, length);
+
+			outputStream.flush();
+			outputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void restoreDatabases(Uri uri) {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		executorService.execute(() -> {
+			Log.d(TAG, "restoreDatabase: start");
+			CategoryDatabase.instance.clearAllTables();
+			SubCategoryDatabase.instance.clearAllTables();
+			AccountDatabase.instance.clearAllTables();
+			TransactionDatabase.instance.clearAllTables();
+			Log.d(TAG, "restoreDatabase: tables cleared");
+
+			mainActivity.runOnUiThread(() -> {
+				try {
+					InputStream inputStream = requireActivity().getContentResolver().openInputStream(uri);
+					ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+					ZipEntry zipEntry;
+
+					// Loop through each entry in the ZIP file
+					while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+						String entryName = zipEntry.getName();
+						entryName = entryName.substring(0, entryName.length() - 3);
+						Log.d(TAG, "restoreDatabases: entryName=" + entryName);
+						restoreDatabase(zipInputStream, entryName);
+						zipInputStream.closeEntry();
+					}
+					zipInputStream.close();
+
+					mainActivity.transactionROOM();
+					mainActivity.accountROOM();
+					mainActivity.categoryROOM();
+					mainActivity.subCatROOM();
+					mainActivity.subCategoryROOM(0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
 		});
+	}
+
+	private String timeStamp() {
+		Calendar calendar = Calendar.getInstance();
+		return calendar.get(Calendar.YEAR) + "_" + (calendar.get(Calendar.MONTH) + 1) + "_" +
+				calendar.get(Calendar.DATE) + "_" + calendar.get(Calendar.HOUR_OF_DAY) +
+				"_" + calendar.get(Calendar.MINUTE) + "_" + calendar.get(Calendar.SECOND);
 	}
 }
